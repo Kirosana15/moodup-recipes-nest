17624c4c442e5633b4a8c6b7c8ab0fa15402cc18
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-11 10:56:39 +0200

    add recipe controller

--- src/recipe/recipe.module.ts
+++ src/recipe/recipe.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { RecipeController } from './recipe.controller';
 
-@Module({})
+@Module({
+  controllers: [RecipeController]
+})
 export class RecipeModule {}

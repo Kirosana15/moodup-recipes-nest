c1200fa4b373d92d0318b6095f96ad7b17c8b447
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-11 11:40:29 +0200

    create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { RecipeModule } from './recipe/recipe.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 import config from '../config/config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [config], isGlobal: true }), UserModule, RecipeModule],
+  imports: [ConfigModule.forRoot({ load: [config], isGlobal: true }), UserModule, RecipeModule, AuthModule],
   controllers: [],
   providers: [],
 })

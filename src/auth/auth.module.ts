38f6d2bcf0ca116f68bb50608ffa9d035809883d
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-11 15:51:49 +0200

    add jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UserModule } from '../user/user.module';
+import { TOKEN_KEY } from './auth.constants';
 import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AuthService]
+  imports: [UserModule, PassportModule, JwtModule.register({ secret: TOKEN_KEY, signOptions: { expiresIn: '60m' } })],
+  providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}

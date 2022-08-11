590955f432e23361115767c1c53765a1be60a236
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-11 10:30:57 +0200

    add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
+import { UserModule } from './user/user.module';
 import config from '../config/config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [config], isGlobal: true })],
+  imports: [ConfigModule.forRoot({ load: [config], isGlobal: true }), UserModule],
   controllers: [],
   providers: [],
 })

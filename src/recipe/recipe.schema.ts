fbca3bde2e6eb85fba24d31e6b073d6c9bc63398
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-11 10:47:28 +0200

    add explicit type to recipe props

--- src/recipe/recipe.schema.ts
+++ src/recipe/recipe.schema.ts
@@ -5,20 +5,20 @@ export type RecipeDocument = Recipe & Document;
 
 @Schema()
 export class Recipe {
-  @Prop({ required: true })
+  @Prop({ required: true, type: 'string' })
   ownerId: string;
 
-  @Prop({ required: true })
+  @Prop({ required: true, type: 'string' })
   title: string;
 
-  @Prop({ default: 'https://picsum.photos/id/1062/350/150?blur=1' })
+  @Prop({ default: 'https://picsum.photos/id/1062/350/150?blur=1', type: 'string' })
   imageUrl: string;
 
-  @Prop({ required: true })
+  @Prop({ required: true, type: 'string' })
   body: string;
 
-  @Prop({ required: true })
-  createdAt: Date;
+  @Prop({ default: Date.now(), type: 'number' })
+  createdAt: number;
 }
 
 export const RecipeSchema = SchemaFactory.createForClass(Recipe);

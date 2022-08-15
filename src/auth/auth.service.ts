52f5ccf6c5c11193eaf7bab84f7c63b5aad5d7bd
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-15 12:11:02 +0200

    fix auth.service.register needlessly calling db twice

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,18 +4,25 @@ import bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { UserCredentialsDto } from '../user/dto/user.credentials.dto';
 import { User } from '../user/user.schema';
+import { Error, MongooseError } from 'mongoose';
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UserService, private jwtService: JwtService) {}
 
-  async register(userCredentialsDto: UserCredentialsDto): Promise<User> {
+  async register(userCredentialsDto: UserCredentialsDto): Promise<User | undefined> {
     const { username, password } = userCredentialsDto;
-    if (await this.userService.getByUsername(username)) {
-      throw new ConflictException('Username taken');
+    try {
+      const hashedPassword = await this.hashPassword(password);
+      const user = await this.userService.create({ username, password: hashedPassword });
+      return user;
+    } catch (err: unknown) {
+      if ((err as { code: number }).code === 11000) {
+        throw new ConflictException('Username taken');
+      } else {
+        throw err;
+      }
     }
-    const hashedPassword = await this.hashPassword(password);
-    return this.userService.create({ username, password: hashedPassword });
   }
 
   private async hashPassword(password: string): Promise<string> {

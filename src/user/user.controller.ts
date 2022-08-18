594a30e4c111a4160d22b50e1899ac75c6e94833
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-18 14:22:47 +0200

    add controller method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UserInfoDto } from './dto/user-from-db.dto';
+import { UserService } from './user.service';
 
+@ApiTags('user')
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @ApiResponse({ isArray: true })
+  @Get('/all')
+  getAllUsers(@Query('limit') limit?: number, @Query('page') page?: number): Promise<UserInfoDto[]> {
+    return this.userService.getAll(limit, page);
+  }
+}

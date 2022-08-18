f7b6ed661d7b031034a8db17fe5ff93bc3808cb7
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-18 13:11:15 +0200

    temp

--- src/auth/test/auth.controller.spec.ts
+++ src/auth/test/auth.controller.spec.ts
@@ -1,18 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from '../auth.controller';
+import { AuthService } from '../auth.service';
+import request from 'supertest';
+import { NestApplication } from '@nestjs/core';
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
+  let authService: AuthService;
+  let getNewTokensSpy: jest.SpyInstance;
+  let app: NestApplication;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
+    getNewTokensSpy = jest.spyOn(authService, 'getNewTokens');
+    app = module.createNestApplication();
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(authController).toBeDefined();
+  });
+
+  it('should return new set of tokens', async () => {
+    const res = await request(app.getHttpServer()).get('auth/refresh-token');
   });
 });

6a4ceaaf022fca16db48d9c5841cd1ea551281a6
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-25 14:53:38 +0200

    remove unnecessary imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 
@@ -6,7 +6,6 @@ import { UserModule } from '../user/user.module';
 import { EXPIRES_IN, TOKEN_KEY } from './auth.constants';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { BasicStrategy } from './strategies/basic.strategy';
 import { BearerStrategy } from './strategies/bearer.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 

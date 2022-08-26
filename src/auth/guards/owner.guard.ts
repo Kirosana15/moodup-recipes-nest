77ebe73a486cb0487dfd21aee731f5c1917cab38
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-26 12:01:46 +0200

    add ownerGuard

---
+++ src/auth/guards/owner.guard.ts
@@ -0,0 +1,15 @@
+import { CanActivate, ExecutionContext } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+import { RoleTypes } from '../enums/roles';
+
+export class OwnerGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const req = context.switchToHttp().getRequest();
+    const { user, params } = req;
+    if (user?._id == params._id || user?.roles.includes(RoleTypes.Admin)) {
+      return true;
+    }
+    return false;
+  }
+}

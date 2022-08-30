092062fd660027ef1a4a1c6482daca7a29b60bd1
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-30 14:50:47 +0200

    add recipeService.delete

--- src/recipe/recipe.service.ts
+++ src/recipe/recipe.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { PaginatedQueryDto } from '../dto/queries.dto';
@@ -8,6 +8,10 @@ import { Recipe, RecipeDocument } from './recipe.schema';
 @Injectable()
 export class RecipeService {
   constructor(@InjectModel(Recipe.name) private recipeModel: Model<RecipeDocument>) {}
+
+  delete(id: string): Promise<RecipeDto | null> {
+    return this.recipeModel.findByIdAndDelete(id).lean().exec();
+  }
 
   searchInTitle(query: string, paginatedQueryDto?: PaginatedQueryDto): Promise<RecipeDto[]> {
     const page = paginatedQueryDto?.page || 1;

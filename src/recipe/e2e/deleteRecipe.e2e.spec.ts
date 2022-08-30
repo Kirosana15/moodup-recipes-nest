319d7e41ddc7446b5234f94f8ea4b934ac9d2a65
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-30 15:05:57 +0200

    fix: :bug: fix /DELETE :id comparing user id to recipe id instead of ownerId

--- src/recipe/e2e/deleteRecipe.e2e.spec.ts
+++ src/recipe/e2e/deleteRecipe.e2e.spec.ts
@@ -5,9 +5,10 @@ import request from 'supertest';
 import { BearerAuthGuard } from '../../auth/strategies/bearer.strategy';
 import { rootMongooseTestModule } from '../../mock/db.mock';
 import { generateUserFromDb } from '../../user/test/mock/user.model.mock';
+import { RecipeDto } from '../dto/recipe.dto';
 import { RecipeModule } from '../recipe.module';
 import { RecipeService } from '../recipe.service';
-import { mockId } from '../test/mock/recipe.mock';
+import { mockId, recipeMock } from '../test/mock/recipe.mock';
 import { mockRecipeService } from '../test/mock/recipeService.mock';
 
 describe('recipe', () => {
@@ -33,6 +34,8 @@ describe('recipe', () => {
 
     app = module.createNestApplication();
     await app.init();
+
+    recipeService.getById.mockImplementation((id: string) => recipeMock({ _id: id, ownerId: mockUser._id }));
   });
 
   afterAll(async () => {
@@ -41,13 +44,14 @@ describe('recipe', () => {
 
   describe('/DELETE :id', () => {
     it('should return deleted recipe', async () => {
-      const idMock = mockUser._id;
+      const idMock = mockId();
       const res = await request(app.getHttpServer()).delete(`/recipe/${idMock}`).expect(HttpStatus.OK);
       expect(res.body).toBeDefined();
       expect(res.body._id).toEqual(idMock);
     });
     it(`should return ${HttpStatus.UNAUTHORIZED} if user is not an owner of a recipe`, async () => {
       const idMock = mockId();
+      recipeService.getById.mockResolvedValueOnce(recipeMock({ _id: idMock, ownerId: mockId() }));
       await request(app.getHttpServer()).delete(`/recipe/${idMock}`).expect(HttpStatus.UNAUTHORIZED);
     });
   });

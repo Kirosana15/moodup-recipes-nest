14855d6685a12805ced48706175ae334efa479b1
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-30 14:52:19 +0200

    add recipeController.deleteRecipe

--- src/recipe/recipe.controller.ts
+++ src/recipe/recipe.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Delete, Get, Param, NotFoundException, Query } from '@nestjs/common';
 import { PaginatedQueryDto } from '../dto/queries.dto';
-import { RecipeDto } from './dto/recipe.dto';
+import { RecipeDto, RecipeIdDto } from './dto/recipe.dto';
 
 import { RecipeService } from './recipe.service';
 
 @Controller('recipe')
 export class RecipeController {
   constructor(private recipeService: RecipeService) {}
+
+  @Delete(':_id')
+  async deleteRecipe(@Param() params: RecipeIdDto): Promise<RecipeDto> {
+    const recipe = await this.recipeService.delete(params._id);
+    if (recipe) {
+      return recipe;
+    }
+    throw new NotFoundException('Recipe does not exist');
+  }
 
   @Get('/search/:query')
   searchRecipeTitles(

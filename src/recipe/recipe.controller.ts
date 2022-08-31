2b039ea424c77c812daaedb78622bb4681f1bd8c
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-31 10:48:33 +0200

    add authorization

--- src/recipe/recipe.controller.ts
+++ src/recipe/recipe.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { RecipeDto } from './dto/recipe.dto';
 
 import { RecipeService } from './recipe.service';
@@ -7,6 +7,7 @@ import { RecipeService } from './recipe.service';
 export class RecipeController {
   constructor(private recipeService: RecipeService) {}
 
+  @UseGuards(bearerAuthGuard)
   @Get('/search/:query')
   searchRecipeTitles(@Query('query') query: string): Promise<RecipeDto[]> {
     return this.recipeService.searchInTitle(query);

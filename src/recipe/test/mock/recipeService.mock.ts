834d0027fdf361216b3258ebd86d893efce84c2b
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-31 12:59:37 +0200

    test: :white_check_mark: add e2e tests

--- src/recipe/test/mock/recipeService.mock.ts
+++ src/recipe/test/mock/recipeService.mock.ts
@@ -4,4 +4,5 @@ export const mockRecipeService = {
   getById: jest.fn().mockImplementation((_id: string) => recipeMock({ _id })),
   delete: jest.fn().mockImplementation((_id: string) => recipeMock({ _id })),
   searchInTitle: jest.fn().mockImplementation((query: string) => generateRecipes(5, { title: query })),
+  update: jest.fn().mockImplementation((id: string, recipe: Partial<RecipeDto>) => recipeMock({ _id: id, ...recipe })),
 };

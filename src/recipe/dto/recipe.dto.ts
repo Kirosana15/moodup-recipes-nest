2e7815a37a99840032a372e13fddf9fb3ef254f3
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-31 10:48:33 +0200

    add recipe DTO

---
+++ src/recipe/dto/recipe.dto.ts
@@ -0,0 +1,27 @@
+import { Optional } from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsMongoId, IsUrl, Length } from 'class-validator';
+
+export class RecipeDto {
+  @ApiProperty()
+  @IsMongoId()
+  ownerId: string;
+
+  @ApiProperty()
+  @Length(3, 20)
+  title: string;
+
+  @ApiProperty()
+  @Optional()
+  @IsUrl()
+  imageUrl: string;
+
+  @ApiProperty()
+  @Length(20, 1000)
+  body: string;
+
+  @ApiProperty()
+  @Optional()
+  @IsDate()
+  createdAt: number;
+}

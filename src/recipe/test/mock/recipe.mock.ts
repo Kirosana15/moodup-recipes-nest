0663926f93f99f178f74d655437dc260bf61eb48
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-31 10:48:33 +0200

    extend functionality of recipeMock

--- src/recipe/test/mock/recipe.mock.ts
+++ src/recipe/test/mock/recipe.mock.ts
@@ -3,14 +3,18 @@ import { faker } from '@faker-js/faker';
 import { generateMockId } from '../../../user/test/mock/user.model.mock';
 import { RecipeDto } from '../../dto/recipe.dto';
 
-export const mockTitle = faker.word.adverb() + faker.word.adjective() + faker.word.noun();
-export const mockImageUrl = faker.image.food();
-export const mockContent = faker.lorem.paragraph(4);
+export const mockTitle = () => faker.word.adverb() + faker.word.adjective() + faker.word.noun();
+export const mockImageUrl = () => faker.image.food();
+export const mockContent = () => faker.lorem.paragraph(4);
 
-export const recipeMock: RecipeDto = {
-  ownerId: generateMockId(),
-  title: mockTitle,
-  imageUrl: mockImageUrl,
-  content: mockContent,
-  createdAt: faker.date.past().getTime(),
+export const recipeMock = (recipe?: Partial<RecipeDto>): RecipeDto => ({
+  ownerId: recipe?.ownerId || generateMockId(),
+  title: recipe?.title || mockTitle(),
+  imageUrl: recipe?.imageUrl || mockImageUrl(),
+  content: recipe?.content || mockContent(),
+  createdAt: recipe?.createdAt || faker.date.past().getTime(),
+});
+
+export const generateRecipes = (count: number, recipe?: Partial<RecipeDto>): RecipeDto[] => {
+  return Array.from(new Array(count), () => recipeMock(recipe));
 };
--- src/user/test/create-user.service.spec.ts
+++ src/user/test/create-user.service.spec.ts
@@ -2,7 +2,7 @@ import { MongooseModule, getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Model } from 'mongoose';
 
-import { closeConnections, rootMongooseTestModule } from '../helpers/db.mock';
+import { closeConnections, rootMongooseTestModule } from '../../mock/db.mock';
 import { User, UserDocument, UserSchema } from '../user.schema';
 import { UserService } from '../user.service';
 import { mockCredentials, mockUsername } from './mock/user.model.mock';

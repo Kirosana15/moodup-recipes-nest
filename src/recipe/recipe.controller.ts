46f0a27a7ccc89e112c2d0408f61adb9e33dcc03
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-31 10:48:33 +0200

    add RecipeController.searchRecipeTitles

--- src/recipe/recipe.controller.ts
+++ src/recipe/recipe.controller.ts
@@ -1,8 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { RecipeDto } from './dto/recipe.dto';
 
 import { RecipeService } from './recipe.service';
 
 @Controller('recipe')
 export class RecipeController {
   constructor(private recipeService: RecipeService) {}
+
+  @Get('/search/:query')
+  searchRecipeTitles(@Query('query') query: string): Promise<RecipeDto[]> {
+    return this.recipeService.searchInTitle(query);
+  }
 }

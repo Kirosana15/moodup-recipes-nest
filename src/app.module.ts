a6286944df250d458f0b8db009f549993f054bef
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-09-05 14:18:39 +0200

    feat: :sparkles: add prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
-import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { BearerAuthGuard } from './auth/guards/bearer.guard';
 import { RolesGuard } from './auth/guards/roles.guard';
+import { PrismaModule } from './prisma/prisma.module';
 import { RecipeModule } from './recipe/recipe.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(process.env.DB_URI || 'mongodb://localhost:27017/test'),
-    UserModule,
-    RecipeModule,
-    AuthModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, RecipeModule, AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [
     {
--- src/db.constants.ts
+++
@@ -1,7 +0,0 @@
-export const DB = {
-  HOST: process.env.DB_HOST || 'localhost',
-  PORT: parseInt(process.env.DB_PORT || '5432'),
-  USER: process.env.DB_USER || 'postgres',
-  PASSWORD: process.env.DB_PASSWORD || 'postgres',
-  NAME: process.env.DB_NAME || 'recipesApi',
-};

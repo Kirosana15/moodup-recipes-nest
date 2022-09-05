f44b481a198c271c890ba582b54f0124a96e10b5
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-09-05 11:49:55 +0200

    test: :white_check_mark: add authorization test

--- src/user/test/e2e/getUser.e2e.spec.ts
+++ src/user/test/e2e/getUser.e2e.spec.ts
@@ -10,11 +10,10 @@ import { generateUser } from '../mock/user.model.mock';
 import { mockUserService } from '../mock/user.service.mock';
 
 describe('user', () => {
-  let service: UserService;
   let app: NestApplication;
   let mockUser: UserInfoDto;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     mockUser = generateUser();
     const module: TestingModule = await Test.createTestingModule({
       imports: [],
@@ -26,28 +25,42 @@ describe('user', () => {
         },
       ],
     }).compile();
-    service = module.get(UserService);
     app = module.createNestApplication();
-    app.useGlobalPipes(new ValidationPipe({ transform: true, transformOptions: { enableImplicitConversion: true } }));
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
     app.useGlobalGuards({
-      canActivate(context) {
+      canActivate(context: ExecutionContext) {
+        const req = context.switchToHttp().getRequest();
+        if (!req.headers['authorization']) {
+          return false;
+        }
         return true;
       },
     });
     await app.init();
   });
-  afterEach(async () => {
+  afterAll(async () => {
     await app.close();
   });
   describe('/GET :id', () => {
     const TEST_PATH = '/user/';
     it(`should return ${HttpStatus.OK} and user information`, async () => {
-      const res = await request(app.getHttpServer()).get(`${TEST_PATH}${mockUser._id}`).expect(HttpStatus.OK);
+      const res = await request(app.getHttpServer())
+        .get(`${TEST_PATH}${mockUser._id}`)
+        .set('Authorization', 'token')
+        .expect(HttpStatus.OK);
       expect(res.body._id).toEqual(mockUser._id);
     });
+
+    it(`should return ${HttpStatus.FORBIDDEN} if user is not authenticated`, async () => {
+      await request(app.getHttpServer()).get(`${TEST_PATH}${mockUser._id}`).expect(HttpStatus.FORBIDDEN);
+    });
+
     it(`should return ${HttpStatus.NOT_FOUND} when user does not exist`, async () => {
       mockUserService.getById.mockReturnValueOnce(null);
-      await request(app.getHttpServer()).get(`${TEST_PATH}${mockUser._id}`).expect(HttpStatus.NOT_FOUND);
+      await request(app.getHttpServer())
+        .get(`${TEST_PATH}${mockUser._id}`)
+        .set('Authorization', 'token')
+        .expect(HttpStatus.NOT_FOUND);
     });
   });
 });

776b9e446d25dbca10efe9a1799ce33bbd6deb91
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-09-05 13:35:18 +0200

    add recipeController.getAllRecipes

--- src/recipe/recipe.controller.ts
+++ src/recipe/recipe.controller.ts
@@ -12,6 +12,10 @@ import { RecipeService } from './recipe.service';
 @Controller('recipe')
 export class RecipeController {
   constructor(private recipeService: RecipeService) {}
+  @Get('/all')
+  getAllRecipes(): Promise<RecipeDto[]> {
+    return this.recipeService.getAll();
+  }
 
   @Post('/')
   createRecipe(@Body() recipeContents: RecipeContentDto, @AuthorizedUser() user: UserInfoDto): Promise<RecipeDto> {

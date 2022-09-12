33306304535d3e6f8cb392cd369d95e0774b2bbe
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-09-12 10:27:10 +0200

    refactor: :coffin: remove old code

--- src/user/constants.ts
+++
@@ -1,5 +0,0 @@
-export const Select = Object.freeze({
-  UserInfo: { id: true, username: true, roles: true, createdAt: true },
-  User: {},
-  UserWithRecipes: { recipes: { select: { title: true, imageUrl: true } } },
-});
